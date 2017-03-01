3a014110aed95784c56b1ae9f7babba0016fd95c
kiran <kirankumarkn76@gmail.com>
2017-03-01 12:24:24 +0530

    Updated ngAnimate in  appjs

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 (function () {
     'use strict';
-    var app = angular.module('studiobuzz', ['ngRoute', 'ui.bootstrap',
+    var app = angular.module('studiobuzz', ['ngRoute','ngAnimate','ui.bootstrap',
         'mgo-angular-wizard', 'ngFileUpload', 'slick', 'ui.thumbnail','ngRateIt',
-        'ngSanitize', 'ngAnimate', 'com.2fdevs.videogular',
+        'ngSanitize', 'com.2fdevs.videogular',
         'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay',
         'com.2fdevs.videogular.plugins.poster', 'com.2fdevs.videogular.plugins.buffering', 'vButton', 'ezfb']);
 })();
-
