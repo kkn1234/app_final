07adc2d2b15de614e6950e6a3dd4f3b483d1aa06
kiran <kirankumarkn76@gmail.com>
2017-03-04 09:35:48 +0530

    redesign - nav, home

--- public/app.js
+++ public/app.js
@@ -4,5 +4,5 @@
         'mgo-angular-wizard', 'ngFileUpload', 'slick', 'ui.thumbnail','ngRateIt',
         'ngSanitize', 'com.2fdevs.videogular',
         'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay',
-        'com.2fdevs.videogular.plugins.poster', 'com.2fdevs.videogular.plugins.buffering', 'vButton', 'ezfb']);
+        'com.2fdevs.videogular.plugins.poster', 'com.2fdevs.videogular.plugins.buffering', 'vButton', 'ezfb', 'ngGridPanel']);
 })();
